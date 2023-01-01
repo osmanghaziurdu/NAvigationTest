148c1a67cf516b61521e3f6d674eb0cd2bdcd458
Osama Jutt <rizvidevelopers@gmail.com>
2023-01-02 01:31:24 +0500

    First

---
+++ app/src/main/java/com/example/navigationtest/MessageFragment.java
@@ -0,0 +1,25 @@
+package com.example.navigationtest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+public class MessageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+
+
+        return inflater.inflate(R.layout.fragment_message, container, false);
+
+
+    }
+}
